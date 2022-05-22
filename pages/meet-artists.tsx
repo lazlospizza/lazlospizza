5dee5e3aec0306472f9ce50f87a5172fa4680401
Night Fox <johnnyh961@gmail.com>
2022-05-21 23:29:49 -0500

    fix menu not fully visible on meet page

--- pages/meet-artists.tsx
+++ pages/meet-artists.tsx
@@ -4,7 +4,7 @@ import { Artist } from '../components/Artist';
 export default function MeetArtists() {
   return (
     <Box p="20px" w="full">
-      <Stack m="10px">
+      <Stack minH="500px" m="10px">
         <Text color="tomato.500" fontWeight={700} fontSize={'xl'}>
           Meet Artist
         </Text>
