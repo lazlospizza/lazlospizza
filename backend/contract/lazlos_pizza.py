5ce7b97122e7e2034eacada2d843b08edb0b4f8a
brandoneprice31 <brandoneprice31@gmail.com>
2022-05-21 18:05:26 -0700

    updated contract

--- backend/contract/lazlos_pizza.py
+++ backend/contract/lazlos_pizza.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 import json
 
-from config.config import WEB3_PROVIDER_URI, CONTRACT_ADDRESS
+from config.config import WEB3_PROVIDER_URI, PIZZA_CONTRACT_ADDRESS
 
 w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URI))
 
@@ -11,7 +11,7 @@ with open("./contract/LazlosPizza.json") as f:
     info_json = json.load(f)
     abi = info_json["abi"]
 
-    adress = Web3.toChecksumAddress(CONTRACT_ADDRESS)
+    adress = Web3.toChecksumAddress(PIZZA_CONTRACT_ADDRESS)
     lazlos_pizza = w3.eth.contract(address=adress, abi=abi)
 
 def pizza(token_id):
