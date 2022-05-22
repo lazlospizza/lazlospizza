c2aedc265628119c05fed316121879c975797327
brandoneprice31 <brandoneprice31@gmail.com>
2022-05-21 23:37:19 -0700

    fix payout

--- backend/payout/calculate.py
+++ backend/payout/calculate.py
@@ -3,12 +3,9 @@ import requests
 def calculate_payout(addr, block):
     response = requests.get(f'https://lazlos-pizza.s3.amazonaws.com/payouts/rinkeby.json')
     payout_data = response.json()
-
-    print(payout_data)
     
     eligible_payouts = payout_data[addr]
     for payout in eligible_payouts:
-        print(payout)
         if payout['block'] == block:
             return payout['payout_amount']
     
