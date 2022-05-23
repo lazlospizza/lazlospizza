1175d3cc023365e1f9c65a2143ee1797e06cd2ee
Night Fox <Johnnyh961@gmail.com>
2022-05-22 19:32:43 -0500

    update artists

--- components/Artist.tsx
+++ components/Artist.tsx
@@ -1,22 +1,41 @@
-import { Box, Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import Link from 'next/link';
 
-interface ArtistProps {
+export interface ArtistProps {
   name: string;
-  about: string;
+  role: string;
+  bio?: string;
+  link?: string;
+  imgSrc?: string;
 }
 
-export const Artist = ({ name, about }: ArtistProps) => {
+export const Artist = ({ bio, name, role, imgSrc, link }: ArtistProps) => {
   return (
     <Box className="artist-card">
       <Flex>
-        <img src="/assets/default-artist.svg" alt="artist-photo" />
+        <Center w="200px" minH="200px">
+          <img
+            src={imgSrc || '/assets/default-artist.svg'}
+            alt="artist-photo"
+            style={{ minWidth: '200px', minHeight: '200px' }}
+          />
+        </Center>
+
         <Stack mx="20px">
           <Heading color="tomato.500" size={'md'}>
-            Artist Name
+            {name}
           </Heading>
-          <Text color="gray.dark" fontWeight={500} fontSize={'lg'}>
-            {`I am an artist. I love art. Check out my work`}
+          <Text color="gray.light" fontWeight={600} fontSize={'lg'}>
+            {role}
           </Text>
+          <Text color="gray.dark" fontWeight={500} fontSize={'lg'}>
+            {bio}
+          </Text>
+          {link && (
+            <Link href={link}>
+              <a>{link}</a>
+            </Link>
+          )}
         </Stack>
       </Flex>
     </Box>
