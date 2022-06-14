cfc469f321de0152eaa08b46fcb9fbab46b90118
Taras Melnychuk <tarasmelnychuk@Tarass-Mac-mini.local>
2022-06-14 20:14:59 +0200

    Added FAQ content

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,11 +1,64 @@
-import { Box, Center, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Text,
+  Center,
+  AccordionIcon,
+} from '@chakra-ui/react';
+import { faqItems } from '../utils/faq';
 
 export default function Faq() {
   return (
-    <Box>
-      <Center h="600px">
-        <Heading color="tomato.500">Coming Soon</Heading>
+    <Box p="20px" w="full">
+      <Center mb={10} mt={6}>
+        <Heading color="tomato.500">FAQ</Heading>
       </Center>
+      <Accordion allowMultiple>
+        {faqItems.map((item, idx) => (
+          <AccordionItem key={idx} mb={5}>
+            <Heading color="tomato.500" cursor="pointer">
+              <AccordionButton
+                px={4}
+                py={3}
+                borderWidth={['1px', '2px']}
+                borderColor="tomato.500"
+                backgroundColor="cheese.100"
+                borderStyle="solid"
+                _hover={{
+                  backgroundColor: 'cheese.100',
+                }}
+                fontSize={[18, 24]}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                {item.question}
+                <AccordionIcon ml={5} fontSize={30} />
+              </AccordionButton>
+            </Heading>
+            <AccordionPanel
+              borderWidth={['1px', '2px']}
+              borderColor="tomato.500"
+              borderStyle="solid"
+              borderTop="none"
+              p={0}
+            >
+              <Text
+                color="gray.dark"
+                p={4}
+                fontSize={[18, 22]}
+                fontWeight={600}
+              >
+                {item.answer}
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </Box>
   );
 }
---
+++ utils/faq.ts
@@ -0,0 +1,18 @@
+export const faqItems: { question: string; answer: string }[] = [
+  {
+    question: `Question 1`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+  {
+    question: `Question 2`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+  {
+    question: `Question 3`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+  {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+];
