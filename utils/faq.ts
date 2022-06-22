0dd91ab3679e691c7e70902e3c22e8f6b6f1350a
when-lambo <100303769+when-lambo@users.noreply.github.com>
2022-06-22 15:01:31 +0100

    Update faq.ts

--- utils/faq.ts
+++ utils/faq.ts
@@ -1,11 +1,15 @@
 export const faqItems: { question: string; answer: string }[] = [
   {
-    question: `Question 1`,
-    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+    question: `Who is Lazlo?`,
+    answer: `Laszlo Hanyecz is the Bitcoin Pizza Guy that bought two pizzas for 10,000 BTC in May 2010 in what is believed to have been the very first purchase of physical goods using the cryptocurrency. This project is named in his honor (sorry, we kinda bastardised the spelling by dropping the ‘s’ after a straw poll asking some people to spell it out). Totally Lazlo, our mascot is a chef-ified version of Totally, a Blitmap original artwork designed by, you guessed it, @Totally.`,
   },
   {
-    question: `Question 2`,
-    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+    question: `What’s the TL:DR for the game?`,
+    answer: `In short:
+•	Bake a pizza in the pizza cave,
+•	Get a lower score than the Score to Beat,
+•	Claim the Rarity Reward at the end of the cycle.
+Bake a pizza in the Pizza Cave, check your Rarity Score in My Wallet after baking – if you get the lowest score overall in the Reward Cycle (see Score to Beat above) you can claim your Rarity Reward when the reward block rolls around. If at first you don’t manage to get a lower score than the Score to Beat, use the Check Rarity tool when Rebaking your pizza to improve your score.`,
   },
   {
     question: `Question 3`,
@@ -15,4 +19,37 @@ export const faqItems: { question: string; answer: string }[] = [
     question: `Question 4`,
     answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
   },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+   {
+    question: `Question 4`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore earum quae consequatur nemo minima. Enim, ratione commodi, modi, officiis consectetur amet exercitationem incidunt aliquid cupiditate id ipsum in laboriosam.`,
+  },
+  
 ];
